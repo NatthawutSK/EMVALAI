501281727828d9a4f00280832463b20128fbf266
Squid3rd <88415664+Squid3rd@users.noreply.github.com>
2023-11-12 18:52:16 +0700

    update Last UI
    ddd

--- src/app/(site)/calendar/page.tsx
+++ src/app/(site)/calendar/page.tsx
@@ -245,12 +245,12 @@ export default function Home() {
 
   return (
     <>
-      <nav className="flex justify-between mb-12 border-b border-violet-100 p-4">
+      <nav className="flex justify-between mb-6 border-b border-blue-100 p-8">
         <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>
       </nav>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="min-h-screen flex-col items-center px-16">
         <div className="grid grid-cols-10">
-          <div className="col-span-8">
+          <div className="col-span-10">
             <FullCalendar
               plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
               headerToolbar={{
@@ -269,9 +269,10 @@ export default function Home() {
               eventClick={(data) => handleDeleteModal(data)}
             />
           </div>
-          <div
+          
+          {/* <div
             id="draggable-el"
-            className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50"
+            className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-blue-50"
           >
             <h1 className="font-bold text-lg text-center">Drag Event</h1>
             {events.map((event) => (
@@ -283,7 +284,7 @@ export default function Home() {
                 {event.title}
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
 
         <Transition.Root show={showDeleteModal} as={Fragment}>
@@ -418,7 +419,7 @@ export default function Home() {
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 
                             shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
                             focus:ring-2 
-                            focus:ring-inset focus:ring-violet-600 
+                            focus:ring-inset focus:ring-blue-600 
                             sm:text-sm sm:leading-6 mb-3"
                               value={newEvent.title}
                               onChange={(e) => handleChange(e)}
@@ -430,7 +431,7 @@ export default function Home() {
                               className="block w-full rounded-md border-0 py-1.5 text-gray-900 
                             shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
                             focus:ring-2 
-                            focus:ring-inset focus:ring-violet-600 
+                            focus:ring-inset focus:ring-blue-600 
                             sm:text-sm sm:leading-6"
                               value={newEvent.description}
                               onChange={(e) => handleChange2(e)}
@@ -440,7 +441,7 @@ export default function Home() {
                           <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                             <button
                               type="submit"
-                              className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                              className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 sm:col-start-2 disabled:opacity-25"
                               disabled={newEvent.title === ""}
                             >
                               Create
